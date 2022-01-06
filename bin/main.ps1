b783184aa9be9971dcd7471c5b1feeeb46d445e9
unknown <ruuiisu>
2022-01-06 11:53:05 +0100

    pull man 2

--- bin/main.ps1
+++ bin/main.ps1
@@ -1,13 +1,21 @@
-$backupPfad = "..\TopBck\"
-$sourcePfad = "..\TopSrc\*"
-$logName = Get-Date -Format 'dd-MM-yyyy HH-mm-ss'
-$logPfad =  "..\log\" + $logName + ".log"
+# Backupskript
+# 06.01.2022
+# Luis Gjokaj
+# main.ps1
+# Version 1.5 (nach 5 mal bearbeiten der Finalversion)
+
+$backupPfad = "..\TopBck\" # Standartpfad fuer das Backup
+$sourcePfad = "..\TopSrc\*" # StandartPfad fuer die Source
+$logName = Get-Date -Format 'dd-MM-yyyy HH-mm-ss' # Name der Log-Datei (Jetzige Zeit und heutiges Datum)
+$logPfad =  "..\log\" + $logName + ".log" #Pfad der LogDatei
 
 Write-Host "Rufen Sie makeBackup um das Backup zu starten."
 
+# Erstellt ein vollstaendiges Backup einer Directory. Keine Parameter.
 function makeBackup {
     $answer = Read-Host "Wollen Sie die Pfaede fuer Backup, source, und log aendern?(n/y) "
 
+    # Verschiedne Pfaede werden ueberschrieben falls man das will. Die auswahl wird mit if else geprueft.
     if ($answer -eq "y") {
         $logpfad = Read-Host "Geben sie den gewuenschten Pfad fuer das Logfile an: "
         $backupPfad = Read-Host "Geben sie den gewuenschten Pfad fuer das Backup an: "
@@ -16,15 +24,39 @@ function makeBackup {
         Write-Host "Falsche Eingabe. Programm wird geschlossen."
         exit
     }
+
+    # Falls nur ein Pfad geandert worden ist, werden die anderen wieder zurueck gestellt auf den Standartpfad.
+    if ($backupPfad -eq "") {
+        $backupPfad = "..\TopBck\"
+    }
+    if ($logPfad -eq "") {
+        $logPfad =  "..\log\" + $logName + ".log"
+    }
+    if ($sourcePfad -eq "") {
+        $sourcePfad = "..\TopSrc\*"
+    }
+
+    # Alle files im Backupverzeichnis werden geloescht und das Backup wird durchgefuehrt
     try {
         Remove-Item -Recurse -Force -Exclude "TopBck" $backupPfad -ErrorAction stop
         copy-item -Path $sourcePfad -Destination $backupPfad -Recurse -ErrorAction stop
     }
     catch {
-        write-Host "Alle Files sind gleich."
+        write-Host "Es ist ein Fehler Aufgetreten. Fuehren Sie das Skript bitte wieder aus."
     }
+
+    # Log Datei wird erstellt und ausgegeben.
     get-childitem -path $sourcePfad -Recurse | Select-Object name | out-file -FilePath $logpfad
-    if ((get-childitem -path $backupPfad -Recurse | Select-Object name) -eq (get-childitem -path $sourcePfad -Recurse | Select-Object name)) {
+
+    # SourcePfad wird umgeaendert fuer pruefungszwecke.
+    $sourcePfad = "..\TopSrc\"
+
+    # Daten ueber die Verzeichnisse werden hier gespeichert.
+    $backupFiles = (get-childitem -path $backupPfad -Recurse)
+    $sourceFiles = (get-childitem -path $sourcePfad -Recurse)
+
+    # Geprueft ob die Dokumentanzahl gleich ist. Falls ja, ist das Backup erfolgreich durchgefuehrt worden.
+    if ($backupFiles.count -eq $sourceFiles.count) {
         write-host "Das Backup wurde erfolgreich durchgefuehrt"
     }
     else {
